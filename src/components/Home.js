a3e0f670995139f074c91baa865f2156ee002900
rajatgatade <97111225+rajatgatade@users.noreply.github.com>
2023-03-27 14:47:44 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+const isLoggedIn = () => {
+    const username = localStorage.getItem('username');
+    const password = localStorage.getItem('password');
+    if (username === 'abc' && password === '123')
+        return true
+    return false;
+
+};
+
 function Home() {
+    const [link, stLink] = useState('/login')
+    useEffect(() => {
+        if (isLoggedIn()) {
+            stLink('/profile')
+        }
+    })
     return (
         <div>
             <ul>
@@ -11,7 +26,7 @@ function Home() {
                     </Link>
                 </li>
                 <li>
-                    <Link id="profile" to="/profile">
+                    <Link id="profile" to={link}>
                         Profile
                     </Link>
                 </li>
