3d189a4c59db9ca7cdaba87ca359f65cb4bbe889
rajatgatade <97111225+rajatgatade@users.noreply.github.com>
2023-03-27 14:49:42 +0530

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,1 +1,12 @@
 
+import React from "react";
+import { Route } from "react-router-dom";
+
+const PrivateRoute = ({ component: Profile, ...rest }) => (
+    <Route {...rest} render={(props) => (
+        <Profile {...props} />
+    )} />
+  );
+
+
+export default PrivateRoute;
